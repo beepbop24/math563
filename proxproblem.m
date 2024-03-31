ffff4c2057f37116bd619758c618cc25a24d30e4
beepbop24 <81869542+beepbop24@users.noreply.github.com>
2024-03-30 21:04:14 -0400

    added correct gamma output

--- proxproblem.m
+++ proxproblem.m
@@ -1,13 +1,16 @@
-function norm_prox = proxproblem(problem)
+function [norm_prox, gamma] = proxproblem(problem, i)
     % function that specifies what norm to use for the prox of g(y1)
-    % INPUT: problem type (l1 or l2)
-    % OUTPUT: method that applies correct prox operator 
+    % INPUTS: problem type (l1 or l2), input parameter values i
+    % OUTPUTS: method that applies correct prox operator and correct gamma
+    % to use
 
     if strcmp(problem, 'l1') == 1
         norm_prox = @(x, lambda, b) l1Prox(x, lambda, b);
+        gamma = i.gammal1;
 
     elseif strcmp(problem, 'l2') == 1
         norm_prox = @(x, lambda, b) l2Prox(x, lambda,b);
+        gamma = i.gammal2;
 
     else
        error('Not a Valid Algorithm')
