d25c250331ae0e3bcba85a99e6fa986b143ebb82
beepbop24 <81869542+beepbop24@users.noreply.github.com>
2024-03-30 22:17:26 -0400

    Update optsolver.m

--- optsolver.m
+++ optsolver.m
@@ -1,21 +1,21 @@
-function x_sol = optsolver(problem, algorithm, x_initial, kernel, b, i)
+function [x_sol, summary] = optsolver(problem, algorithm, x_initial, kernel, b, i)
 
     % selects correct norm & gamma to use for the function g (l1 or l2)
     [norm_prox, gamma] = proxproblem(problem);
 
     if strcmp(algorithm, 'douglasrachfordprimal') == 1
         % need to decompose x_initial to right format for this algorithm
-        deblurred_x = primaldr(b, i.tprimaldr, i.rhoprimaldr, gamma, i.maxiter, x_initial, kernel, norm_prox);
+        x_sol = primaldr(b, i.tprimaldr, i.rhoprimaldr, gamma, i.maxiter, x_initial, kernel, norm_prox);
 
     elseif strcmp(algorithm, 'douglasrachfordprimaldual') == 1
         % need to decompose x_initial to right format for this algorithm
-        deblurred_x = primaldualdr(b, i.tprimaldualdr, i.rhoprimaldualdr, gamma, i.maxiter, x_initial, kernel, norm_prox);
+        x_sol = primaldualdr(b, i.tprimaldualdr, i.rhoprimaldualdr, gamma, i.maxiter, x_initial, kernel, norm_prox);
 
     elseif strcmp(algorithm, 'admm') == 1
 
     elseif strcmp(algorithm, 'chambollepock') == 1
         % need to decompose x_initial to right format for this algorithm
-        deblurred_x = chambollepock(b, i.tcp, i.scp, gamma, i.maxiter, x_initial, kernel, norm_prox);
+        x_sol = chambollepock(b, i.tcp, i.scp, gamma, i.maxiter, x_initial, kernel, norm_prox);
 
     else
         error('Not a Valid Algorithm')
