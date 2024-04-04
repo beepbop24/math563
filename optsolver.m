a3062f81912a7dcc39ee7792e1a70e79c6847b8d
beepbop24 <81869542+beepbop24@users.noreply.github.com>
2024-04-04 18:16:04 -0400

    completed optsolver

--- optsolver.m
+++ optsolver.m
@@ -1,26 +1,54 @@
-function [x_sol, summary] = optsolver(problem, algorithm, x_initial, x_original, kernel, b, i, tol)
+function [x_sol, summary_x] = optsolver(problem, algorithm, x_initial, x_original, kernel, b, i)
     % function that computes given algorithm to deblur image
-    % INPUTS: type of problem ... needs commenting...
+    % INPUTS: problem ('l1' or 'l2' indicating the norm to use), 
+    % algorithm (algorithm of choice from "douglasrachfordprimal",
+    % "douglasrachfordprimaldual", "admm", and "chambollepock")
+    % x_initial: initial guess of the correct image
+    % x_original: true image
+    % b: blurred & noisy image
+    % i (structure containing the following hyperparameters:
+    %  i.maxiter: maximum number of iterations
+    %  i.gammal1, i.gammal2: denoising parameter for the l1 and l2 problems
+    %  i.rhoprimaldr, i.tprimaldr: relaxation parameter 0 < rho < 2 and
+    %  stepsize t > 0 used by the primal Douglas Rachford splitting algorithm
+    %  i.rhoprimaldualdr, i.tprimaldualdr: elaxation parameter 0 < rho < 2
+    %  and stepsize t > 0 used by the primal-dual Douglas Rachford splitting
+    %  algorithm
+    %  i.rhoadmm, i.tadmm: elaxation parameter 0 < rho < 2 and
+    %  stepsize t > 0 used by the ADMM algorithm
+    %  i.tcp, i.scp: step size parameters t>0, s>0 used by the Chambolle
+    %  Pock algorithm
+    %  i.tol: tolerance used to measure convergence of the loss
+    %  i.analysis: boolean that specifies whether the algorithm prints
+    %  info during each iteration)
+    %
+    % OUTPUTS: deblurred_x (deblurred image), summary_x (containing objective
+    % function value, # of iterations, CPU time, loss)
 
     % selects correct norm & gamma to use for the function g (l1 or l2)
-    [norm_prox, gamma] = proxproblem(problem);
+    [norm_prox, gamma] = proxproblem(problem, i);
 
+    % starts timer
+    timerstart = tic;
+
+    % choice of algorithm
     if strcmp(algorithm, 'douglasrachfordprimal') == 1
-        % need to decompose x_initial to right format for this algorithm
-        [x_sol, k, loss] = primaldr(b, x_original, i.tprimaldr, i.rhoprimaldr, gamma, i.maxiter, tol, x_initial, kernel, norm_prox);
+        [x_sol, k, loss] = primaldr(b, x_original, i.tprimaldr, i.rhoprimaldr, gamma, i.maxiter, i.tol, i.analysis, x_initial, kernel, norm_prox);
 
     elseif strcmp(algorithm, 'douglasrachfordprimaldual') == 1
-        % need to decompose x_initial to right format for this algorithm
-        [x_sol, k, loss] = primaldualdr(b, x_original, i.tprimaldualdr, i.rhoprimaldualdr, gamma, i.maxiter, tol, x_initial, kernel, norm_prox);
+        [x_sol, k, loss] = primaldualdr(b, x_original, i.tprimaldualdr, i.rhoprimaldualdr, gamma, i.maxiter, i.tol, i.analysis, x_initial, kernel, norm_prox);
 
     elseif strcmp(algorithm, 'admm') == 1
-        % something
+        [x_sol, k, loss] = admm(b, x_original, i.tadmm, i.rhoadmm, gamma, i.maxiter, i.tol, i.analysis, x_initial, kernel, norm_prox);
 
     elseif strcmp(algorithm, 'chambollepock') == 1
-        % need to decompose x_initial to right format for this algorithm
-        [x_sol, k, loss] = chambollepock(b, x_original, i.tcp, i.scp, gamma, i.maxiter, tol, x_initial, kernel, norm_prox);
+        [x_sol, k, loss] = chambollepock(b, x_original, i.tcp, i.scp, gamma, i.maxiter, i.tol, i.analysis, x_initial, kernel, norm_prox);
 
     else
         error('Not a Valid Algorithm')
     end
+
+
+    timerend = toc(timerstart);
+    summary_x = summary(x_sol, b, gamma, kernel, k, i.maxiter, loss, timerend, i.tol);
 end
