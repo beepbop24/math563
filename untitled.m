7ff4c4194a0675c888b43796337bd5fd5d08fe50
beepbop24 <81869542+beepbop24@users.noreply.github.com>
2024-04-04 18:11:41 -0400

    all tests now with optsolver

--- untitled.m
+++ untitled.m
@@ -1,18 +1,7 @@
 
-%% CODE TO TEST ALGORITHMS
-% cameraman -- importing image + kernel and noise
-
-image_x = imread("testimages/cameraman.jpg");
-image_x = rgb2gray(image_x);
-
-image_x = double(image_x(:, :, 1));
-mn = min(image_x(:));
-image_x = image_x - mn;
-mx = max(image_x(:));
-image_x = image_x/mx;
-
-%resizefactor = 0.1;
-%image_x = imresize(image_x, resizefactor);
+%% DIFFERENT IMAGES & KERNELS TO TEST ALGORITHMS
+% img 1 -- most tests (small image)
+image_x = importimage("testimages/cameraman.jpg");
 
 kernel = fspecial('gaussian', [10 10], 15);
 
@@ -25,6 +14,11 @@ imshow(image_x,[])
 
 figure('Name','image before deblurring')
 imshow(b,[])
+
+
+% img 2 -- bigger image
+%resizefactor = 0.1;
+%image_x = imresize(image_x, resizefactor);
 
 %% DEFAULT PARAMETER VALUES
 % maximum number of iterations
@@ -42,18 +36,21 @@ i.tprimaldr = 0.1;
 % relaxation parameter (rho in (0, 2)) and step size t > 0 for Primal Dual
 % Douglas-Rachford Splitting
 i.rhoprimaldualdr = 1.5;
-i.tprimaldualdr = 0.05;
+i.tprimaldualdr = 0.1;
+
+% relaxation parameter (rho in (0, 2)) and step size t > 0 for Alternating
+% Direction Method of Multipliers
+i.rhoadmm = 1.5;
+i.tadmm = 0.1;
 
 % step size parameters for the Chambolle-Pock Algorithm
 i.tcp = 0.1;
 i.scp = 0.1;
 
 
-%% TEST ALGO 1
-[applyK, applyD1, applyD2, applyKTrans, applyD1Trans, applyD2Trans, invertMatrix] = multiplyingMatrix(b, kernel, i.tcp);
-
+%% TESTING ALL ALGORITHMS
 [m, n] = size(b);
-z1_0 = image_x; %rand(m, n);
+z1_0 = rand(m, n);
 z21_0 = applyK(z1_0);
 z22_0 = applyD1(z1_0);
 z23_0 = applyD2(z1_0);
@@ -62,10 +59,24 @@ z2_0 = cat(3, z21_0, z22_0, z23_0);
 
 problem = 'l1';
 [norm_prox, gamma] = proxproblem(problem, i);
-tol = 10;
+i.tol = 10;
+i.analysis = true;
+i.maxiter=10;
 
-[deblurred_x, k, loss] = chambollepock(b, image_x, 0.01, 0.01, 0.5, i.maxiter, tol, z1_0, z2_0, z1_0, kernel, norm_prox);
+[deblurred_x1, summary1] = optsolver('l1', 'douglasrachfordprimal', z1_0, image_x, kernel, b, i);
+[deblurred_x2, summary2] = optsolver('l1', 'douglasrachfordprimaldual', z1_0, image_x, kernel, b, i);
+[deblurred_x3, summary3] = optsolver('l1', 'admm', z1_0, image_x, kernel, b, i);
+[deblurred_x4, summary4] = optsolver('l1', 'chambollepock', z1_0, image_x, kernel, b, i);
 
 figure();
-imshow(deblurred_x,[])
+imshow(deblurred_x1,[])
+
+figure();
+imshow(deblurred_x2,[])
+
+figure();
+imshow(deblurred_x3,[])
+
+figure();
+imshow(deblurred_x4,[])
      
