0ae45145dff9de18ac38e1055fab04fa94cd515e
beepbop24 <81869542+beepbop24@users.noreply.github.com>
2024-04-04 18:10:51 -0400

    fixed typo in objective function

--- objectivefunction.m
+++ objectivefunction.m
@@ -8,8 +8,8 @@ function f = objectivefunction(x, b, gamma, kernel)
     % here u=1 is unused
     [applyK, applyD1, applyD2] = multiplyingMatrix(b, kernel, 1);
 
-    if all(x(:) < 1) && all(x(:) > 0)
-         % first term is l1 norm of Kx-b, second term is gamma * iso norm of Dx 
+    if all(x(:) <= 1) && all(x(:) >= 0)
+        % first term is l1 norm of Kx-b, second term is gamma * iso norm of Dx 
         % (in this case the indicator function is 0)
         f=norm(applyK(x)-b, 1)+gamma*sum(sqrt(applyD1(x).^2+applyD2(x).^2), "all");
 
