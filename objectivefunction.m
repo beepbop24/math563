2df17586008cc8a7f81c55e5305231ae173ef07c
beepbop24 <81869542+beepbop24@users.noreply.github.com>
2024-04-09 22:47:11 -0400

    corrected small mistake

--- objectivefunction.m
+++ objectivefunction.m
@@ -1,4 +1,4 @@
-function f = objectivefunction(x, b, gamma, kernel)
+function f = objectivefunction(x, b, gamma, kernel, problem)
     % function that computes the value of the objective function for x
     % INPUTS: partially deblurred x, blurred image b, blurring kernel and
     % gamma used for isoprox
@@ -9,11 +9,21 @@ function f = objectivefunction(x, b, gamma, kernel)
     [applyK, applyD1, applyD2] = multiplyingMatrix(b, kernel, 1);
 
     if all(x(:) <= 1) && all(x(:) >= 0)
-        % first term is l1 norm of Kx-b, second term is gamma * iso norm of Dx 
+        % first term is l1 or l2 norm of Kx-b, second term is gamma * iso norm of Dx 
         % (in this case the indicator function is 0)
-        f=norm(applyK(x)-b, 1)+gamma*sum(sqrt(applyD1(x).^2+applyD2(x).^2), "all");
+        if strcmp(problem, 'l1') == 1
+            norm_term = norm(applyK(x)-b, 1);
+
+        elseif strcmp(problem, 'l2') == 1
+            norm_term = norm(applyK(x)-b, 2)^2;
+        end
+        
+
+        f = norm_term + gamma*sum(sqrt(applyD1(x).^2+applyD2(x).^2), "all");
 
     else
+        % pixel values are not between 0 and 1 so indicator function is +
+        % infinity
         f = Inf;
     end
 end
