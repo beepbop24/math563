c04cb7a206109329357b70403cbcecb47fa7a338
beepbop24 <81869542+beepbop24@users.noreply.github.com>
2024-04-09 22:53:44 -0400

    added description

--- testinggamma.m
+++ testinggamma.m
@@ -1,9 +1,18 @@
 function [deblurred_x, gamma_loss] = testinggamma(problem, algorithm, x_initial, x_original, kernel, b, i, gamma_values)
+    % function that tests performance of algorithm on various values of gamma
+    % INPUTS: type of problem (l1 or l2), type of algorithm ("douglasrachfordprimal",
+    % "douglasrachfordprimaldual", "admm", and "chambollepock"), initial
+    % guess x_initial, original image x_original, kernel, blurred image b,
+    % i parameter values and gamma values to iterate over
+    % OUTPUTS: loss array with loss for each value of gamma and last deblurred
+    % image
+
     n = length(gamma_values);
     gamma_loss = zeros(1, n);
 
     for k=1:n
         i.gammal1 = gamma_values(k);
+        i.gammal2 = gamma_values(k);
         [deblurred_x, summary] = optsolver(problem, algorithm, x_initial, x_original, kernel, b, i);
 
         gamma_loss(k) = summary.loss;
--- testingscp.m
+++ testingscp.m
@@ -1,4 +1,12 @@
 function [deblurred_x, s_loss] = testingscp(problem, x_initial, x_original, kernel, b, i, s_values)
+    % function that tests performance of algorithm on various values of s
+    % for Chambolle-Pock algorithm
+    % INPUTS: type of problem (l1 or l2), initial
+    % guess x_initial, original image x_original, kernel, blurred image b,
+    % i parameter values and s values to iterate over
+    % OUTPUTS: loss array with loss for each value of s and last deblurred
+    % image
+
     n = length(s_values);
     s_loss = zeros(1, n);
 
