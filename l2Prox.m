66a6eb9345554c8bb6a3db9ec7d8b352f0b24e9f
beepbop24 <81869542+beepbop24@users.noreply.github.com>
2024-04-12 16:27:04 -0400

    corrected typo (doesn't change output)

--- l2Prox.m
+++ l2Prox.m
@@ -2,5 +2,5 @@ function prox_x = l2Prox(x,lambda, b)
     % function that computes the prox of the l2-norm of (x-b)
     % INPUT: current image x, blurred image b and lambda (step-size here)
     % OUTPUT: prox of (x-b)
-    prox_x = (x+2*lambda*b)./(1+2*lambda);
+    prox_x = (x+2*lambda*b)/(1+2*lambda);
 end
